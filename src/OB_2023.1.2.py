1a42294a015ef40e225d04b1846313579a1a5f90
AH-321 <116464553+AH-321@users.noreply.github.com>
2024-07-11 22:17:34 -0700

    Update OB_2023.1.2.py
    Made it less shit.

--- src/OB_2023.1.2.py
+++ src/OB_2023.1.2.py
@@ -2,6 +2,9 @@ import discord
 import python_weather
 import asyncio
 import os
+import time
+import subprocess
+
 
 intents = discord.Intents.default()
 intents.presences = True
@@ -9,43 +12,76 @@ intents.members = True
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+def getDirectory():
+    # get file's dir
+    return os.path.dirname(os.path.realpath(__file__))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+try:
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        time.sleep(1)
+        os.system('clear')
 
-    elif message.content.startswith('$about'):
-        await message.channel.send('I am a prototype bot created by OptiByte Software Solutions.')
+        with open(getDirectory() + "/logo.py") as f:
+            exec(f.read())
 
-    elif message.content.startswith('$help'):
-        await message.channel.send('As I am still in development, the list of commands has not yet been published.')
+        print("Starting logging...")
+        time.sleep(4)
+        print("LOG status:", log_status())
+        print("Starting bot...")
+        log_status()
+        time.sleep(2)
+        print("########## Ready ##########")
+        #get_time()
+except:
+    print("ERROR: Could not start bot.")
 
-    elif message.content.startswith('$kill'):
-        exit()
+try:
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
 
-    elif message.content.startswith('$gettemp'):
-        await get_temp(message)  # await get_weather coroutine
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
 
-    elif message.content.startswith('$getforecast'):
-        await get_forecast(message)
+        elif message.content.startswith('$about'):
+            await message.channel.send('I am a prototype bot created by OptiByte Software Solutions.')
+
+        elif message.content.startswith('$help'):
+            print("LOG: user has sent '$help")
+            await message.channel.send('You can get weather data (for Salmon Arm) by typing "$gettemp", or "$getforecast". More fetures will be added.')
+
+        elif message.content.startswith('$kill'):
+            print("LOG: user has sent '$kill'")
+            print("suspending logging...")
+            time.sleep(2)
+            print("Shutting down...")
+            os.system('clear')
+            exit()
+
+        elif message.content.startswith('$gettemp'):
+            await get_temp(message)  # await get_weather coroutine
+
+        elif message.content.startswith('$getforecast'):
+            await get_forecast(message)
+except:
+    print("ERROR: Could not send message.")
 
 async def get_temp(message):
+    print("LOG: user has sent '$gettemp'")
     client = python_weather.Client()
     response = await client.get('Salmon Arm')
 
     await message.channel.send(f'Temperature in Salmon Arm: {response.current.temperature}°C')
 
 
-    await client.close()  #close session
+    await client.close()
 
 async def get_forecast(message):
+    print("LOG: user has sent '$getforecast'")
     client = python_weather.Client()
     response = await client.get('Salmon Arm')
 
@@ -54,5 +90,15 @@ async def get_forecast(message):
 
     await client.close()
 
+def log_status():
+    log_status = "OK"
+    return log_status
 
-client.run('TOKEN')
+def global_status():
+    print(ProcessLookupError)
+
+def get_time():
+    with open(getDirectory() + "/gets_time.py") as f:
+        exec(f.read())
+
+client.run('TOKEN') 
